21ccbb8ad53fda99e5f6a77735903748f46b4335
Matias Camiletti <matias.camiletti@gmail.com>
2024-09-27 21:45:42 -0300

    Create blank project.

---
+++ src/mc-schematic/index.ts
@@ -0,0 +1,12 @@
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function mcSchematic(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    // Aquí puedes definir la lógica para generar archivos o modificar el proyecto
+    tree.create('hello.txt', 'Hello, World!');
+    return tree;
+  };
+}
