2f6ca2c6f010fab20a57f2a6ba74bac37cbafff5
Matias Camiletti <matias.camiletti@gmail.com>
2025-03-14 22:00:52 -0300

    Add function to update app.config.ts with new imports and providers

--- src/init/index.ts
+++ src/init/index.ts
@@ -17,11 +17,30 @@ function runCommand(command: string, _context: SchematicContext) {
 }
 
 function createFileWithTemplate(tree: Tree, fileName: string, template: string) {
+  // Verify if exist the file
+  if (tree.exists(fileName)) {
+    return;
+  }
+
   const filePath = path.join(__dirname, 'files/' + template);
   if (fs.existsSync(filePath)) {
     let content = fs.readFileSync(filePath, 'utf8');
     tree.create(fileName, content);
   }
+}
+
+function insertStringAt(originalString: string, stringToInsert: string, position: number): string {
+  if (position > originalString.length) {
+    // Si la posición está fuera del rango, simplemente agrega el string al final
+    return originalString + stringToInsert;
+  }
+
+  // Dividir el original en dos partes: antes y después de la posición
+  const before = originalString.slice(0, position);
+  const after = originalString.slice(position);
+
+  // Insertar el string en la posición deseada
+  return before + stringToInsert + after;
 }
 
 function processStyles(tree: Tree, _context: SchematicContext) {
@@ -53,6 +72,46 @@ body, html {
   tree.overwrite(filePath, updatedContent);
 }
 
+function processAppConfig(tree: Tree, _context: SchematicContext) {
+  const filePath = 'src/app/app.config.ts';
+
+  if(!tree.exists(filePath)){
+    return;
+  }
+
+  const fileBuffer = tree.read(filePath);
+  let content = fileBuffer ? fileBuffer.toString() : '';
+
+  // Search position of the last import
+  const importPosition = content.lastIndexOf('import');
+  // Search position of the last ; after import position
+  const importEndPosition = content.indexOf(';', importPosition);
+  // Add the new import after the last import
+  content = insertStringAt(content, `
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';`, importEndPosition + 1);
+
+  // Search position of the last ]
+  const providerPosition = content.lastIndexOf(']');
+  // Add the new provider after the last provider
+  content = insertStringAt(content, `,
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.dark',
+          cssLayer: false
+        }
+      }
+    })`, providerPosition);
+
+  tree.overwrite(filePath, content);
+}
+
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function init(_options: any): Rule {
@@ -71,6 +130,9 @@ export function init(_options: any): Rule {
 
     // Create .postcssrc.json file from template
     createFileWithTemplate(tree, 'tailwind.config.js', 'tailwind.config.js.template');
+
+    // Config app.config.ts
+    processAppConfig(tree, _context);
 
     return tree;
   };
