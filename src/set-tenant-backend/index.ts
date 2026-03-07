91e7f5fbdd99687f63577bf73ec1e8626963bcbf
Matias Camiletti <matias.camiletti@gmail.com>
2026-03-07 17:24:26 -0300

    feat: Add HTTP client providers, authentication, and tenant interceptors to `app.config.ts`.

--- src/set-tenant-backend/index.ts
+++ src/set-tenant-backend/index.ts
@@ -3,6 +3,44 @@ import { runCommand } from '../core/run-command';
 import { addRoute } from '../core/add-route';
 import { getContentByFile } from '../core/get-content-by-file';
 import { addSourceInStyles } from '../core/add-source-in-styles';
+import { insertStringAt } from '../core/insert-string-at';
+
+function processAppConfig(tree: Tree, _context: SchematicContext) {
+  const filePath = 'src/app/app.config.ts';
+
+  if (!tree.exists(filePath)) {
+    return;
+  }
+
+  const fileBuffer = tree.read(filePath);
+  let content = fileBuffer ? fileBuffer.toString() : '';
+
+  // Search position of the last import
+  const importPosition = content.lastIndexOf('import');
+  // Search position of the last ; after import position
+  const importEndPosition = content.indexOf(';', importPosition);
+  // Add the new import after the last import
+  content = insertStringAt(content, `
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+
+import { authInterceptor, provideMCAuth } from '@mckit/auth';
+
+import { tenantInterceptor } from '@mckit/tenant';
+
+import { environment } from '../environments/environment';`, importEndPosition + 1);
+
+  // Replace the zoneChangeDetectionStrategy
+  content = content.replace('provideZoneChangeDetection({ eventCoalescing: true }),', `provideZoneChangeDetection({ eventCoalescing: true }),
+    provideMCAuth({
+      baseUrl: environment.baseUrl
+    }),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor, tenantInterceptor])
+    ),,`);
+
+  tree.overwrite(filePath, content);
+}
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -68,6 +106,9 @@ export function install(_options: any): Rule {
   },`, tree, _context, `import { tenantListGuard } from '@mckit/tenant';
 import { mcAuthGuard } from '@mckit/auth';
 import { MCFuseLayoutComponent } from '@mckit/layout-fuse';`);
+
+    // Config app.config.ts
+    processAppConfig(tree, _context);
 
     return tree;
   };
